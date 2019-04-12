0aaf7a23b55ffa63b6c255cff201b85ece0b46b5
udhaykumarbala <udhaykumar97@outlook.com>
2019-04-12 16:24:36 +0530

    change

---
+++ Num.java
@@ -0,0 +1,12 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        int n = args[0];
+        for(i=0;i<n;i++){
+        	System.out.println("Hello");
+        }
+    } 
+} 
