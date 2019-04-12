d580df1bfb0acc31a411be14650d12fd92856597
udhaykumarbala <udhaykumar97@outlook.com>
2019-04-12 15:50:28 +0530

    initial

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        int number = args[0];
+        if(args[0]==0){
+        	System.out.println("zero");
+        } 
+        else if (args[0]>0) {
+
+        	System.out.println("positive");
+        }
+        else{
+        	System.out.println("negative");
+        }
+    } 
+} 
