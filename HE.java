f6a6d2ffc0ff4c64165b9ec45221f46675a0b14a
udhaykumarbala <udhaykumar97@outlook.com>
2019-05-16 17:57:07 +0530

    Update HE.java

--- HE.java
+++ HE.java
@@ -1,1 +1,11 @@
+class hello{
 
+    public static void main(String args[]) 
+    { 
+        int n = args[0];
+        for(i=0;i<n;i++){
+        	System.out.println("Positive");
+        }
+    } 
+
+}
