e9f1710db41fc420c1128609390b30f1b70af2f3
udhaykumarbala <udhaykumar97@outlook.com>
2019-05-17 12:29:44 +0530

    Update HelloWorl.java

--- HelloWorl.java
+++ HelloWorl.java
@@ -5,8 +5,8 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         int n = args[0];
-        for(i=0;i<n;i++){
-        	System.out.println("Hello");
-        }
+      
+        	System.out.println("5");
+        
     } 
 } 
