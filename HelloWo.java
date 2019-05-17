4922259dc9bab22a8975d4219b53fd9799fafd45
udhaykumarbala <udhaykumar97@outlook.com>
2019-05-17 12:34:13 +0530

    Update HelloWo.java

--- HelloWo.java
+++ HelloWo.java
@@ -4,9 +4,9 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        int n = args[0];
+        
       
-        	System.out.println("1 2 3 5 45");
+        	System.out.println("1 2 3 4 5");
         
     } 
 } 
