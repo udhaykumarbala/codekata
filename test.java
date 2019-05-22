9ee2db22429a4b6cc652869409003b2e0d0d06fa
udhaykumarbala <udhaykumar97@outlook.com>
2019-05-22 12:30:32 +0530

    Update and rename Hello.java to test.java

--- test.java
+++ test.java
@@ -2,10 +2,9 @@ class hello{
 
     public static void main(String args[]) 
     { 
-        int n = args[0];
-        for(i=0;i<n;i++){
+        
         	System.out.println("Hello");
-        }
+       
     } 
 
 }
